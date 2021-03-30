8fe67e4b8ea453b2fb754b7f6c49daceb8a467d2
sirajul777 <thony.raj880@gmail.com>
2021-03-30 08:34:26 +0800

    gojek ui

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:gojek_ui/page/launcher_view.dart';
+import 'package:gojek_ui/constant/color.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: 'NeoSans',
+        primaryColor: GojekPalette.green,
+        accentColor: GojekPalette.green,
+      ),
+      home: new LauncherPage(),
+    );
+  }
+}
